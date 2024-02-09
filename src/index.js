49e220862513441cf841ff79ce890bff446cd832
M. Mutahar <mmutahar38@gamil.com>
2024-02-09 09:32:19 +0500

    connection with front end succecfull

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ const express=require('express');
 const router= require("./Routes/index.js")
 const app=express();
 const port=3003;
-
+const cors = require("cors")
 // app.get('/',(req,res)=>{
 //     res.send('brandify backend run--')
 // });
+
+const corsOptions= {
+    origin : "http://localhost:3000",
+    method : "GET, POST, DELETE, PUT, PATCH, HEAD",
+    credentials : true
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", router); 
-
 connectdb().then(() => 
 {
     app.listen(process.env.PORT,()=>{
